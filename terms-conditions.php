87d828b6743f97a60d74490e8dbc5da8a7fbbd02
jyoti12yyadav <jyoti.prixim@gmail.com>
2024-02-08 18:46:11 +0530

    All

--- terms-conditions.php
+++ terms-conditions.php
@@ -29,17 +29,49 @@
                     <div class="row justify-content-center">
                         <div class="col-xl-12 col-lg-12 col-md-6 col-sm-8">
                             <div class="services-item shine-animate-item">
-                                <div class="services-thumb">
-                                    <a class="shine-animate" href="/services-details"><img src="assets/img/services/5409.jpg" alt=""></a>
-                                </div>
+                                
                                 <div class="services-content">
                                     <h4 class="title">ACCEPTANCE & REVISIONS</h4>
                                     <p>By using Humantroop Ventures, website, you signify your agreement to these Terms of Use in their entirety and without modification. We may revise these terms at any time without prior notice by updating this page and such revisions will be effective upon posting to this page. Please check this page periodically for any changes. Your continued use of this website following the posting of any revisions to these terms will mean you accept those changes.</p>
                                 </div>
+                                <div class="services-content">
+                                    <h4 class="title">COPYRIGHT, TRADEMARK & OTHER INTELLECTUAL PROPERTY</h4>
+                                    <p>This website, and all of the content it contains, or may in the future contain, including but not limited to text, content, photographs, video, audio and graphics, designs, information, applications, software, music, articles, directories, guides, as well as the trademarks, trade names, logos, domain names, code, and/or any other form of intellectual property (collectively, the “Service”) that relates to this website, are owned by or licensed by Humantroop Ventures or our associates and partners and are prohibited from any unauthorized use.</p>
+                                    <p>Permission is granted to download and print materials from this website for the sole purposes of viewing, reading, and retaining for reference. Any other copying, distribution, retransmission, or modification of information or materials on this website, whether in electronic or other form, without our express prior written permission, is strictly prohibited.</p>
+                                </div>
+                                <div class="services-content">
+                                    <h4 class="title">ACCEPTABLE USE</h4>
+                                    <p>You must not use this website in any way that causes, or may cause, damage to the website or impairment of the availability or accessibility of the website; or in any way which is unlawful, illegal, fraudulent or harmful, or in connection with any unlawful, illegal, fraudulent or harmful purpose or activity.</p>
+                                    <p>You must not use this website to copy, store, host, transmit, send, use, publish or distribute any material which consists of (or is linked to) any spyware, computer virus, Trojan horse, worm, keystroke logger, rootkit or other malicious computer software.</p>
+                                    <p>You must not conduct any systematic or automated data collection activities (including without limitation scraping, data mining, data extraction and data harvesting) on or in relation to this website without Humantroop Ventures express written consent.</p>
+                                    <p>You may not use this website to transmit or send unsolicited commercial communications.</p>
+                                </div>
+
+                                <div class="services-content">
+                                    <h4 class="title">RESTRICTED ACCESS</h4>
+                                    <p>Access to certain areas of this website is restricted. Humantroop Ventures reserves the right to restrict access to other areas of this website, or indeed this entire website, at our discretion. If we provide you with a user ID and password to enable you to access restricted areas of this website or other content or services, you must ensure that the user ID and password are kept confidential. We may disable your user ID and password in our sole discretion without notice or explanation.</p>
+                                  
+                                </div>
+
+                                <div class="services-content">
+                                    <h4 class="title">LINKS TO OTHER WEBSITES</h4>
+                                    <p>There are links to other websites on the website of Humantroop Ventures. Such websites are not under the control of Humantroop Ventures, and is not responsible for the content or operation of any linked websites or any link contained in a linked website. Humantroop Ventures does not endorse companies or products to which it links. If you decide to access any of the third-party websites linked to the Humantroop Ventures website, you do so at your own risk. Humantroop Ventures, reserves the right to terminate any link or linking program at any time.</p> 
+                                </div>
+
+                                <div class="services-content">
+                                    <h4 class="title">DISCLAIMER OF WARRANTIES</h4>
+                                    <p>YOU AGREE TO ASSUME TOTAL RESPONSIBILITY AND RISK FOR YOUR USE OF THE SITE, THE SERVICE AND THE CONTENT AND FOR EVALUATING THE ACCURACY, COMPLETENESS AND USEFULNESS OF ALL CONTENT PROVIDED BY THE SITE OR THE SERVICE. HUMANTROOP VENTURES, MAKES NO EXPRESS OR IMPLIED WARRANTIES, REPRESENTATIONS OR ENDORSEMENTS WHATSOEVER WITH RESPECT TO THE WEBSITE, THE SERVICE OR THE CONTENT. Humantroop Ventures EXPRESSLY DISCLAIMS ALL WARRANTIES OF ANY KIND, EXPRESS, IMPLIED, STATUTORY OR OTHERWISE, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NONINFRINGEMENT WITH REGARD TO THE SITE, THE SERVICE, AND THE CONTENT. Humantroop Ventures DOES NOT WARRANT THAT THE FUNCTIONS PERFORMED BY THE SITE OR THE SERVICE WILL BE UNINTERRUPTED, TIMELY, SECURE OR ERROR-FREE, OR THAT DEFECTS IN THE SITE OR THE SERVICE WILL BE CORRECTED. HUMANTROOP VENTURES DOES NOT WARRANT THE ACCURACY OR COMPLETENESS OF THE CONTENT, OR THAT ANY ERRORS IN THE CONTENT WILL BE CORRECTED. THE SITE, THE SERVICE AND THE CONTENT ARE PROVIDED ON AN “AS IS” AND “AS AVAILABLE” BASIS.</p> 
+                                </div>
+
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+
+           
+
+
         </section>
 <?php include("includes/footer.php"); ?>
