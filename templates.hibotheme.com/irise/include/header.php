0e01677022b36557deb0384663dfe57a64ed392c
jyoti12yyadav <jyoti.prixim@gmail.com>
2024-03-11 11:54:37 +0530

    All

---
+++ templates.hibotheme.com/irise/include/header.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="tob-header-area">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-lg-4 col-md-4">
+                    <div class="heder-left-content">
+                        <div class="content">
+                            <i class="ri-user-voice-line"></i>
+                            <p>Welcome To Our Irise Staffing Agency</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-8">
+                    <div class="heder-right-content">
+                        <div class="row align-items-center">
+                            <div class="col-lg-9 col-md-7">
+                                <div class="time-content">
+                                    <i class="ri-time-line"></i>
+                                    <p>Sun-Thu: 10am to 7pm - Sat-Fri: Closed</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-5">
+                                <div class="social-content">
+                                    <ul>
+                                        <li>
+                                            <a href="https://www.facebook.com/" target="_blank"><i class="ri-facebook-line"></i></a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.twitter.com/" target="_blank"><i class="ri-twitter-line"></i></a>
+                                        </li>
+                                        <li>
+                                            <a href="https://instagram.com/?lang=en" target="_blank"><i class="ri-instagram-line"></i></a>
+                                        </li>
+                                        <li>
+                                            <a href="https://linkedin.com/?lang=en" target="_blank"><i class="ri-linkedin-line"></i></a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="navbar-area">
+        <div class="mobile-responsive-nav">
+            <div class="container">
+                <div class="mobile-responsive-menu">
+                    <div class="logo">
+                        <a href="index.html">
+                            <img src="assets/images/logo-icon-1.png" class="logo-icon-1" alt="logo">
+                            <img src="assets/images/logo-icon-2.png" class="logo-icon-2" alt="logo">
+                            
+                            <img src="assets/images/logo.png" class="main-logo" alt="logo">
+                            <img src="assets/images/white-logo.png" class="white-logo" alt="logo">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="desktop-nav">
+            <div class="container-fluid">
+                <nav class="navbar navbar-expand-md navbar-light">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="assets/images/logo.png" class="main-logo" alt="logo">
+                        <img src="assets/images/white-logo.png" class="white-logo" alt="logo">
+                    </a>
+                    <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent" style="display: block;">
+                        <ul class="navbar-nav m-auto">
+                            <li class="nav-item">
+                                <a href="index-2.html" class="nav-link">
+                                    Home            
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="about-us.html" class="nav-link">Who We Are</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="#" class="nav-link dropdown-toggle">
+                                    Services            
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a href="services-style-one.html" class="nav-link">Services Style One</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="services-style-two.html" class="nav-link">Services Style Two</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="service-details.html" class="nav-link">Service Details</a>
+                                    </li>                 
+                                </ul>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="#" class="nav-link dropdown-toggle">
+                                    Portfolio            
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a href="portfolio-style-one.html" class="nav-link">Portfolio Style One</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="portfolio-style-two.html" class="nav-link">Portfolio Style Two</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="portfolio-details.html" class="nav-link">Portfolio Details</a>
+                                    </li>                 
+                                </ul>
+                            </li>
+
+
+                            <li class="nav-item">
+                                <a href="#" class="nav-link dropdown-toggle">
+                                    Pages 
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link dropdown-toggle">
+                                            Team 
+                                        </a>
+    
+                                        <ul class="dropdown-menu">
+                                            <li class="nav-item">
+                                                <a href="team-style-one.html" class="nav-link">Team Style One</a>
+                                            </li>
+    
+                                            <li class="nav-item">
+                                                <a href="team-style-two.html" class="nav-link">Team Style Two</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="pricing.html" class="nav-link">Pricing</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="testimonials.html" class="nav-link">Testimonials</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="faq.html" class="nav-link">FAQ</a>
+                                    </li>
+                                    
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link dropdown-toggle">
+                                            Users 
+                                        </a>
+    
+                                        <ul class="dropdown-menu">
+                                            <li class="nav-item">
+                                                <a href="login.html" class="nav-link">Login</a>
+                                            </li>
+    
+                                            <li class="nav-item">
+                                                <a href="register.html" class="nav-link">Register</a>
+                                            </li>
+
+                                            <li class="nav-item">
+                                                <a href="recover-password.html" class="nav-link">Recover Password</a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="privacy-policy.html" class="nav-link">Privacy Policy</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="terms-conditions.html" class="nav-link">Terms And Conditions</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="coming-soon.html" class="nav-link">Coming Soon</a>
+                                    </li>
+                                    
+                                    <li class="nav-item">
+                                        <a href="404.html" class="nav-link">404 Page</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="#" class="nav-link dropdown-toggle">
+                                    Blog 
+                                </a>
+
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a href="blog-grid.html" class="nav-link">Blog Grid</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="blog-right-sidebar.html" class="nav-link">Blog Right Sidebar</a>
+                                    </li>
+
+                                    <li class="nav-item">
+                                        <a href="blog-details.html" class="nav-link">Blog Details</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li class="nav-item">
+                                <a href="contact-us.html" class="nav-link">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="others-option-for-responsive">
+            <div class="container">
+                <div class="dot-menu">
+                    <div class="inner">
+                        <div class="others-options style2 ms-auto">
+                            <div class="option-item">
+                                <div class="header-contact-box">
+                                    <a href="tel:+0408886666">
+                                        <div class="icon">
+                                            <i class="flaticon-phone-call-1"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="option-item">
+                                <a href="contact-us.html" class="default-btn btn ">Get Started</a>
+                            </div>
+
+                            <div class="option-item style2">
+                                <div class="switch-box">
+                                    <label id="switch2" class="switch">
+                                        <input type="checkbox" onchange="toggleTheme()" id="slider2">
+                                        <span class="slider round"></span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
