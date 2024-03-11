57885a0a6c4b613c5a7df0c4c65e2428016725e6
Ziasolkar <ziaprixim@gmail.com>
2024-03-11 18:13:54 +0530

    replace

--- our-approach.php
+++ our-approach.php
@@ -2,31 +2,102 @@
 
 
 <div class="page-header-area bg-f4fbf6">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6 col-md-6">
-                        <div class="page-header-content">
-                            <h1>Our Approach</h1>
-                            <ul>
-                                <li><a href="index.html">Who We Are</a></li>
-                                <li>Our Approach</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6">
-                        <div class="page-header-image">
-                            <img src="assets/images/banner/banner-img-3.png" alt="Image">
-                        </div>
-                    </div>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6">
+                <div class="page-header-content">
+                    <h1>Our Approach</h1>
+                    <ul>
+                        <li><a href="index.html">Who We Are</a></li>
+                        <li>Our Approach</li>
+                    </ul>
                 </div>
-
-                <div class="page-header-shape">
-                    <img src="assets/images/shape/shape-8.png" alt="Image">
-                    <img src="assets/images/shape/shape-9.png" alt="Image">
-                    <img src="assets/images/shape/shape-10.png" alt="Image">
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="page-header-image">
+                    <img src="assets/images/banner/banner-img-3.png" alt="Image">
                 </div>
             </div>
         </div>
+
+        <div class="page-header-shape">
+            <img src="assets/images/shape/shape-8.png" alt="Image">
+            <img src="assets/images/shape/shape-9.png" alt="Image">
+            <img src="assets/images/shape/shape-10.png" alt="Image">
+        </div>
+    </div>
+</div>
+
+<!--Start About Area-->
+<div class="about-area style-2 ptb-100">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="about-image-content pr-15">
+                    <img src="assets/images/about/about-img-2.png" alt="Image">
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="about-content pl-15">
+                    <div class="about-title">
+                        <h2>Our Approach</h2>
+                        <p>Humantroop Ventures is the leading Manpower Company in Navi Mumbai, providing highly-skilled
+                            personnel to businesses and organizations across the region. Our team of dedicated
+                            professionals has been providing exceptional service for more than a decade, and we remain
+                            committed to delivering the highest quality staffing solutions.</p>
+
+                        <p>Humantroop Ventures takes a holistic approach focused on the belief that people and culture
+                            have a profound impact on the success of any business. We all know there is no "one size
+                            fits all" approach to business. Although every business is unique, we are proud to know that
+                            small, medium and even large businesses work.</p>
+
+                        <p>We provide a strategy to implementation proposition to improve performance and create better
+                            places to work by ensuring that the entire organizational system is aligned and set up to
+                            meet their strategic intent and business objectives. We go beyond questions of structure,
+                            roles, and processes to address elements such as people, behavior, and culture. Our approach
+                            is based on sound change management and well-researched principles of organizational design,
+                            both collaborative and efficient.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--End About Area-->
+
+<!--Start Details Area-->
+<div class="service-details-area pt-100 pb-70">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="services-details-content">
+                    <div class="top-img">
+                        <img src="assets/images/services/services-img-16.jpg" alt="Image">
+                    </div>
+                    <div class="analysis-content">
+                        <p>Our style centers around an unrelenting drive to act with integrity and honesty, along with
+                            an unyielding determination to ensure that the people we work with are empowered and trusted
+                            to deliver the best for our customers. We enjoy getting people's support for ideas and
+                            providing others with goals, plans, and structures that move everyone in the same direction
+                            and help them work toward an overall goal. As we operate at the senior management level and
+                            act as a 'trusted advisor', it is important to demonstrate decisiveness, the ability to take
+                            responsibility for pressing issues and directly challenge others and make critical decisions
+                            in problem areas. It is important to design a delivery style that is engaging, creative, and
+                            strategic, but also 'takes charge', decisive and analytical.</p>
+                            
+                        <p>With a high level of customer satisfaction and a commitment to excellence, Humantroop
+                            Ventures is the go-to Manpower Company in Navi Mumbai. We use an engagement and design
+                            thinking approach that builds ownership among employees and key stakeholders. We have deep
+                            experience in organizational design, alignment, and transformation and have proven
+                            methodologies, but we always try to understand your challenges first before helping you
+                            create a plan. We don't have preconceived notions about how you must do things.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--End Details Area-->
 
 
 <?php include("footer.php"); ?>
