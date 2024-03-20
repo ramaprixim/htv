93475b4000551e32d3e042b28870b245a6ca3303
pranaliprixim <pranalip.prixim@gmail.com>
2024-03-20 12:05:25 +0530

    any

---
+++ disclaimer.php
@@ -0,0 +1,130 @@
+<?php include("header.php") ?>
+
+<div class="page-header-area bg-f4fbf6">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6">
+                <div class="page-header-content">
+                    <h1>Disclaimer</h1>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="page-header-image">
+                    <img src="assets/images/banner/banner-img-3.png" alt="Image">
+                </div>
+            </div>
+        </div>
+
+        <div class="page-header-shape">
+            <img src="assets/images/shape/shape-8.png" alt="Image">
+            <img src="assets/images/shape/shape-9.png" alt="Image">
+            <img src="assets/images/shape/shape-10.png" alt="Image">
+        </div>
+    </div>
+</div>
+
+
+<!--Start Blog Area-->
+<div class="blog-area pt-100 pb-70">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12 col-sm-6" data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200">
+                <div class="single-blog-card style2">
+                    <div class="blog-content">
+                        <h2>Disclaimer</h2>
+                        <p>The information and material published on www.humantroop.in are subject to change without
+                            prior notice. Information contained in this website, including text, graphics, links or
+                            other items are provided on an 'as is', 'as available' basis. Humantroop Ventures does not
+                            warrant the accuracy, adequacy timeliness, authority or completeness of the information and
+                            material and this site may contain technical inaccuracies or typographical errors.
+                            Humantroop Ventures expressly disclaims liability for errors or omissions in this
+                            information and material. No warranty of any kind, implied, express or statutory, including
+                            but not limited to the warranties of non-infringement of third party rights, title,
+                            merchantability, fitness for a particular purpose and freedom from computer virus, is given
+                            in conjunction with the information and materials.
+                        </p>
+                        <p>
+                            Humantroop Ventures has no control over, and no duty to take any action regarding: which
+                            users gain access to its Website and/or the Services; what content they access via the Site
+                            and/or the Services; what effects the content on the Site and/or the Services may have on
+                            the user; how the user may interpret or use the content on the Site and/or the Services; or
+                            what actions the user may take as a result of having been exposed to the content on the Site
+                            and/or the Services. Therefore, in no event will Humantroop Ventures be liable for any
+                            damages, including without limitation direct or indirect, special, incidental, or
+                            consequential damages, losses or expenses arising in connection with this website or use
+                            thereof or inability to use by any party, or in connection with any failure of performance,
+                            error, omission, interruption, defect, delay in operation or transmission, computer virus or
+                            line or system failure, even if Humantroop Ventures, or representatives thereof, are advised
+                            of the possibility of such damages, losses or expenses.
+                        </p>
+                        <p>
+                            The Humantroop Ventures information consists of statements of opinion and not statements of
+                            fact or recommendations to utilize the services of any specific person verified by us. You
+                            should obtain any additional information necessary to make an informed decision prior to
+                            utilizing any specific person/service provider. You assume all responsibility in connection
+                            with choosing any person/service provider, whether or not you obtained information about
+                            such person/service provider on or through Website. Humantroop Ventures and the directors,
+                            officers, agents, and representatives, assume no (and hereby disclaims all) responsibility
+                            or liability of any kind, for any advice, treatment or other services rendered by any
+                            person/service provider, or for any malpractice claims and other claims that may arise
+                            directly or indirectly from any such advice, treatment or other services.
+                        </p>
+                    </div>
+                    <div class="blog-content">
+                        <h2>IMPORTANT NOTE - DISCLAIMER & INDEMNIFICATION</h2>
+                        <p>HUMANTROOP VENTURES, DO NOT charge / accept / receive any amount or security deposit from job
+                            seekers/candidates during the recruitment process or in lieu of job offers. We would like to
+                            bring to your notice that HUMANTROOP VENTURES follows a merit-based employee recruitment
+                            practice with a well-defined screening process.
+                        </p>
+                        <p>
+                            Please be advised that any potential job-seeker willingly corresponding with individuals
+                            claiming and pretending to represent HUMANTROOP VENTURES in any manner whether by e-mails or
+                            otherwise will be doing so at their own risks and consequences. It is strongly recommended
+                            that the potential job-seekers should not respond to such solicitations.
+                        </p>
+                        <p>
+                            HUMANTROOP VENTURES will not accept any liability for any loss or damage suffered / incurred
+                            directly or indirectly through correspondence with such fraudulent and / or unauthorized
+                            individuals and that such communication should not be treated as an offer or representation
+                            from HUMANTROOP VENTURES.
+                        </p>
+                        <p>
+                            Should you come across any such fraudulent incident or have any information regarding
+                            solicitation for recruitment or employment by or on behalf of HUMANTROOP VENTURES, please
+                            report such activities to us at support@humantroop.in
+                        </p>
+                    </div>
+                    <div class="blog-content">
+                        <h2>How to identify fraudulent activities / fake job offers:</h2>
+                        <p>Representatives of HUMANTROOP VENTURES will never send an offer letter from Gmail,
+                            Rediffmail, Yahoo mail, Hotmail etc.Offers will only be from emails coming from domain
+                            humantroop.in and recruitment process will be carried out from www.humantroop.in domain
+                        </p>
+                        <p>
+                            HUMANTROOP VENTURES members will never charge or request for fees from any candidates or
+                            potential job seekers for any purpose during or after the hiring process.
+                        </p>
+                        <p>
+                            HUMANTROOP VENTURES Recruitment team will never ask for your Debit / Credit Card or OTP
+                            details. If you get any unauthorized, suspicious or fraudulent offers or interview call,
+                            please write to us at support@humantroop.in Please be aware of such fraudulent practices and
+                            fraudsters and always email to enquire with us.
+                        </p>
+                        <p>
+                            To all the job-seeker community, we never expect anything from you except ‘Thank You’ ! Our
+                            services are always free for Job Seekers.
+                        </p>
+                        <p>
+                            We Work for You!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--End Blog Area-->
+
+
+<?php include("footer.php") ?>
---
+++ refunds-policy.php
@@ -0,0 +1,82 @@
+<?php include("header.php") ?>
+
+<div class="page-header-area bg-f4fbf6">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6">
+                <div class="page-header-content">
+                    <h1>Refunds Policy</h1>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="page-header-image">
+                    <img src="assets/images/banner/banner-img-3.png" alt="Image">
+                </div>
+            </div>
+        </div>
+
+        <div class="page-header-shape">
+            <img src="assets/images/shape/shape-8.png" alt="Image">
+            <img src="assets/images/shape/shape-9.png" alt="Image">
+            <img src="assets/images/shape/shape-10.png" alt="Image">
+        </div>
+    </div>
+</div>
+
+
+<!--Start Blog Area-->
+<div class="blog-area pt-100 pb-70">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12 col-sm-6" data-aos="fade-up" data-aos-duration="1200" data-aos-delay="200">
+                <div class="single-blog-card style2">
+                    <div class="blog-content">
+                        <h2>1. Refunds Policy</h2>
+                        <p>a. The fee and charges are non-refundable.</p>
+                        <p>b. The fees are to be paid online through the facility made on the Website. Third parties
+                            support and services are required to process online fee payment. Humantroop Venturesis not
+                            responsible for any loss or damage caused to user during this process as these third parties
+                            are beyond the control of Humantroop Ventures.</p>
+                        <p>c. All fees are exclusive of taxes. GST of 18.00% is levied on every purchase.</p>
+                        <p>d. Humantroop Venturesreserves the right to modify the fee structure, either by notice on the
+                            Website or through email to the authorized User, which shall be considered as valid and
+                            agreed communication.</p>
+                    </div>
+                    <div class="blog-content">
+                        <h2>2. TAT</h2>
+                        <p>Humantroop Venturesnormally requires (14-21) working days to obtain the information. This
+                            excludes weekends and holidays. However, some delays may occur which can be beyond our
+                            control. Reasons for such delays are:</p>
+                        <p>a. Incomplete / inadequate information (Insufficient information) is provided or if any
+                            document is missing, you’ll be contacted via mail / call or account update to ask for
+                            required information / document.</p>
+                        <p>b. Delays in Response–Universities /Schools/ Educational Institutes or employers that are
+                            closed for the summer/ election holidays etc.</p>
+                    </div>
+                    <div class="blog-content">
+                        <h2>3. Insufficiency</h2>
+                        <p>In case required information /document is not facilitated in 15 days from the date of report
+                            we will process the case with the given information and will be authorized to close the case
+                            as “Unable To Verify” which will be reflected in the report.</p>
+                        <p>If you have any concerns on progress of your order, contact Humantroop Venturesvia "Contact
+                            us" / Email or Call and we will be happy to assist you.</p>
+                    </div>
+                    <div class="blog-content">
+                        <h2>4. Sharing</h2>
+                        <p>The final report stays in the Humantroop Venturesdatabase for your future use. Also, for your
+                            convenience:</p>
+                        <p>a. Your report will be available online on your account which can be accessed using
+                            respective credentials.</p>
+                        <p>b. If you have SELF VERIFIED yourself Humantroop Ventureswill allow you to SHARE, the report
+                            with anyone on future using “SHARE OPTION” on respective Humantroop Venturesaccount.</p>
+                        <p>c. Your information & your records will become a portable asset for you which you can access
+                            as per your convenience using respective credentials.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--End Blog Area-->
+
+<?php include("footer.php") ?>
